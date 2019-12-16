18d4e91383128afe76c51ef1efc570aa6681cc8e
unknown <sonymaharjan04@gmail.com>
2019-12-15 20:28:41 -0600

    new tests

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,13 +1,68 @@
-import pytest
 import requests
-#
-# def test_url(requests_mock):
-#     requests_mock.get('http://127.0.0.1:5000/', text='data')
-#     assert 'dataaa' == requests.get('http://127.0.0.1:5000/').text
+from bs4 import BeautifulSoup
+import unittest
 
-#test_home()
+url = 'http://127.0.0.1:5000/'
 
-def test_url():
-    url = 'http://127.0.0.1:5000/register'
-    resp = requests.get(url)
-    assert resp.status_code == 200
+def getElemById(text, id):
+    result = BeautifulSoup(text, "html.parser").find(id=id)
+    return result
+
+class MyAppTest(unittest.TestCase):
+
+    def testRegisterPage(self):
+        resp = requests.get(url + 'login')
+        assert resp.status_code == 200
+        
+    def testLoginPage(self):
+        resp = requests.get(url + 'register')
+        assert resp.status_code == 200
+
+    def testSpellCheckPage(self):
+        resp = requests.get(url + 'spell_check')
+        assert resp.status_code == 200
+
+    def testRegister(self):
+        creds = {"username":"JaneDoe", "password":"password", "twofactor":"4021111111" }
+        resp = requests.Session().post(url+'register', data=creds)
+        assert resp.status_code == 200
+
+        result = getElemById(resp.text, "success")
+        #print(result)
+        assert "success" in result.text
+
+    def testRegisterDupUser(self):
+        creds = {"username":"JaneDoe", "password":"password", "twofactor":"4021111111" }
+        resp = requests.Session().post(url+'register', data=creds)
+        assert resp.status_code == 200
+
+        result = getElemById(resp.text, "success")
+        #print(result)
+
+        assert "failure" in result.text
+
+    def testLogin(self):
+        creds = {"username": "JaneDoe", "password": "password", "twofactor": "4021111111"}
+        resp = requests.Session().post(url + 'login', data=creds)
+        assert resp.status_code == 200
+
+        result = getElemById(resp.text, "result")
+        #print(result)
+
+        assert "success" in result.text
+
+    def badLogin(self):
+        creds = {"username": "JaneXDoe", "password": "password", "twofactor": "4021111111"}
+        resp = requests.Session().post(url + 'login', data=creds)
+        assert resp.status_code == 200
+
+        result = getElemById(resp.text, "result")
+        #print(result)
+
+        assert "Incorrect" in result.text
+
+
+    def testSpellCheck(self):
+        words = {"words": "Testword"}
+        resp = requests.Session().post(url + 'spell_check', data=words)
+        assert resp.status_code == 200
